940d6c115907fc7b12fff125b3b65acf8d27e561
madhurendra kumar <106946384+madhurendra-kumar@users.noreply.github.com>
2025-07-22 21:00:53 +0530

    Time: 0 ms (100%), Space: 40.5 MB (67.38%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int res=0;
+        int m=x^y;
+        while(m!=0){
+            if((m&1)==1){
+                res++;
+            }
+            m=m>>1;
+        }
+        return res;
+        
+    }
+}
