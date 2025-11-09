8bff182ae162a801820571c66d033247cf134b6a
madhurendra kumar <106946384+madhurendra-kumar@users.noreply.github.com>
2025-11-09 20:15:04 +0530

    Time: 7 ms (94.09%), Space: 47.1 MB (5.24%) - LeetHub

---
+++ 1260-day-of-the-year/1260-day-of-the-year.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int dayOfYear(String date) {
+        int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8, 10));
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            days[1] = 29;
+        }
+
+        for (int i = 0; i < month - 1; i++) {
+            day += days[i];
+        }
+
+        return day;
+    }
+}
