6f7f4684db8adf1c2401c073a507ef3259d86d69
madhurendra kumar <106946384+madhurendra-kumar@users.noreply.github.com>
2026-02-08 20:59:26 +0530

    Time: 1 ms (30.78%), Space: 45.7 MB (38.96%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,24 +14,17 @@
  * }
  */
 class Solution {
-    public boolean isBalanced(TreeNode root) {
-        if(root == null || root.val == -1) {
-            return true;
-        }
-        return helper(root) != -1;
-    }
+  public boolean isBalanced(TreeNode root) {
+    if (root == null)
+      return true;
+    return Math.abs(maxDepth(root.left) - maxDepth(root.right)) <= 1&& //
+        isBalanced(root.left) &&                                        //
+        isBalanced(root.right);
+  }
 
-    private int helper(TreeNode root) {
-        if(root == null || root.val == -1) {
-            return 0;
-        }
-
-        int left = helper(root.left);
-        int right = helper(root.right);
-        if(left == -1 || right == -1 || Math.abs(left - right) > 1) {
-            return -1;
-        }
-
-        return Math.max(left, right) + 1;
-    }
+  private int maxDepth(TreeNode root) {
+    if (root == null)
+      return 0;
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+  }
 }
